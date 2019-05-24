025ca3e6c3a682759e54c47f74ac563fdb3e9aff
hppRC <hpp.ricecake@gmail.com>
2019-05-24 23:02:13 +0900

    modify quadratic elimination

--- QuadraticEquation.py
+++ QuadraticEquation.py
@@ -3,7 +3,7 @@ def quadraticEquation(a, b, c):
     x1 = (-b + D**(0.5))/(2*a)
     x2 = c / (a*x1)
 
-    return (x1, x2)
+    return [x1, x2]
 
 if __name__ == "__main__":
     print(quadraticEquation(1, 2, 1))
